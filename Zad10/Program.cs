2e1467f4800d02e0bebd812ecf50d0da58b640fe
zaktraxer <zaktraxer@yandex.ru>
2022-12-20 21:03:00 +0300

    Добавлена задача 10

---
+++ Zad10/Program.cs
@@ -0,0 +1,8 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите трехзначное число - ");
+int number = Convert.ToInt32(ReadLine());
+
+int a = (number - (number / 100) * 100) / 10;
+
+WriteLine(a);
