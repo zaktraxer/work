b33936c7cb8fc2cae99ac86eb2b739bfb5666418
zaktraxer <zaktraxer@yandex.ru>
2022-12-20 20:30:06 +0300

    Добавлена задача 4

---
+++ zad4/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число 1 - ");
+int number1 = Convert.ToInt32(ReadLine());
+WriteLine("Введите число 2 - ");
+int number2 = Convert.ToInt32(ReadLine());
+WriteLine("Введите число 3 - ");
+int number3 = Convert.ToInt32(ReadLine());
+if (number1 > number2 && number1 > number3)
+{
+    Console.WriteLine($"Максимальное число {number1}");
+}
+if (number2 > number1 && number2 > number3)
+{
+    Console.WriteLine($"Максимальное число {number2}");
+}
+if (number3 > number1 && number3 > number2)
+{
+    WriteLine($"Максимальное число {number3}");
+}
