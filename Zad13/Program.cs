a65c4f34158efa81ec968f05c168c76f0af28614
zaktraxer <zaktraxer@yandex.ru>
2022-12-20 21:19:01 +0300

    Добавлена задача 13

---
+++ Zad13/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число - ");
+int number = Convert.ToInt32(ReadLine());
+if ((number / 100) > 0)
+{
+    number = (number % 1000) / 100;
+    WriteLine(number);
+}
+else
+{
+    WriteLine("Третьей цифры нет");
+}
+// WriteLine(number % 10 == 0 ? "");
