98604c317fbcffa565ed22b4c82f2b0a93d7d5f8
zaktraxer <zaktraxer@yandex.ru>
2022-12-20 20:49:18 +0300

    Добавлена задача 8

---
+++ Zad8/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число - ");
+int number1 = Convert.ToInt32(ReadLine());
+int n = 0;
+while (n <= number1)
+{
+    WriteLine(n);
+    n = n + 2;
+}
