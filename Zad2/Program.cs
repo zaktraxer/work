66f7d4c6cc5d3e4a6395c10e29547960f03d9830
zaktraxer <zaktraxer@yandex.ru>
2022-12-20 20:00:32 +0300

    Добавлена задача2

---
+++ Zad2/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число 1 - ");
+int number1 = Convert.ToInt32(ReadLine());
+WriteLine("Введите число 2 - ");
+int number2 = Convert.ToInt32(ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"Большее {number1} Меньшее {number2} ");
+}
+else
+{
+    Console.WriteLine($"Большее {number2} Меньшее {number1} ");
+}
