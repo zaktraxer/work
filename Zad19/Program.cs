85854cf47aaf4710e868237398b1d0628300186c
zaktraxer <zaktraxer@yandex.ru>
2022-12-25 14:19:03 +0300

    Добавлена задача 19

---
+++ Zad19/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите пятизначное число - ");
+int number = Convert.ToInt32(ReadLine());
+if (number / 10000 >= 1 && number / 10000 <= 9)
+{
+    int n1 = number / 10000;
+    int n2 = (number / 1000) % 10;
+    int n4 = (number % 100) / 10;
+    int n5 = number % 10;
+
+    if (n1 == n5 && n2 == n4)
+    {
+        Console.WriteLine($"Число {number} палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"Число {number} не пятизначное");
+}
