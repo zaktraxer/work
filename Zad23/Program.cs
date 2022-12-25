5eb1994bbd4954a7a30a1bc51342b3b9ae9a7a4e
zaktraxer <zaktraxer@yandex.ru>
2022-12-25 15:10:06 +0300

    Добавлена задача 23

---
+++ Zad23/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите - ");
+int number = Convert.ToInt32(ReadLine());
+int i = 1;
+while (i <= number)
+{
+    Write(Math.Pow(i, 3));
+    Write(" ");
+    i++;
+}
