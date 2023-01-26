d5926218cb251d56dbbb362b12c9467ab5232fe4
zaktraxer <zaktraxer@yandex.ru>
2023-01-26 10:22:48 +0300

    25 задача

---
+++ Zad25/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите первое число");
+int number1 = int.Parse(ReadLine()!);
+WriteLine("Введите второе число");
+int number2 = int.Parse(ReadLine()!);
+int pow1=GetPow(number1, number2);
+WriteLine($"{number1} В степени {number2} равно {pow1}");
+int GetPow (int A, int B)
+{
+    int result=1;
+    while  (B>0)
+    {
+        result *= A;
+        B--;
+    }
+    return result;
+}
