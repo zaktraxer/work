1698b3c047a776720c618e6807bea094321a2fb8
zaktraxer <zaktraxer@yandex.ru>
2023-01-26 10:43:02 +0300

    27 задача

---
+++ Zad27/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число");
+int number = int.Parse(ReadLine()!);
+int sum = GetSumDigit(number);
+WriteLine($"Сумма цифр числа {number} равна {sum}");
+
+int GetSumDigit (int num)
+{
+    int result=0;
+    int digitcount = (int)Math.Log10(num)+1;
+    while (digitcount > 0)
+    {
+        int a = num % 10;
+        num = num / 10;
+        result=result+a;
+        digitcount --;
+    }
+    return result;
+}
