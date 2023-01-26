ee30f0195d0ea637a05540950d2f9835de6417d0
zaktraxer <zaktraxer@yandex.ru>
2023-01-26 18:14:56 +0300

    29 задача

---
+++ Zad29/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Console;
+using System;
+Clear();
+int [] array = GetArray();
+Printarray(array);
+
+int[] GetArray()
+{
+    int[] res = new int[8];
+    Random rand = new Random();
+    for (int i=0; i<8; i++)
+    {
+        res[i] = rand.Next(100);
+    }
+    return res;
+}
+void Printarray(int[] array)
+{
+    int count=array.Length;
+    for (int i=0; i<count; i++)
+    {
+        Write($"{array[i]} ");
+    }
+}
