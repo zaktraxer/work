e730c1f414871ab0ea148ab9d9b83fcc20f3bda6
zaktraxer <zaktraxer@yandex.ru>
2023-01-29 17:04:59 +0300

    Задача 41

---
+++ Zad41/Program.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+Clear();
+
+int[] array = GetArray();
+Printarray(array);
+int count = Checkpositive(array);
+WriteLine($"Количество четных элементов {count}");
+int[] GetArray()
+{
+    Write("Введите члены массива через пробел ");
+    string[] stroka = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] array = new int[stroka.Length];
+    for (int i=0; i<stroka.Length; i++)
+    {
+        array[i]=int.Parse(stroka[i]);
+    }
+    return array;
+}
+
+int Checkpositive(int[] array)
+{
+    int count = 0;
+    foreach (int a in array)
+    {
+        count+= a>0? 1:0;
+    }
+    return count;
+}
+
+void Printarray(int[] array)
+{
+    int count=array.Length;
+    for (int i=0; i<count; i++)
+    {
+        Write($"{array[i]} ");
+    }
+    WriteLine("");
+}
