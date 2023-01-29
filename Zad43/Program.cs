3c8f601ae62f3b3365c93c0cbfb8c53f9ab8581f
zaktraxer <zaktraxer@yandex.ru>
2023-01-29 18:16:01 +0300

    задача 43

---
+++ Zad43/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+Clear();
+Write("Введите коэффициенты k1, b1, k2, b2 через пробел ");
+string[] stroka = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+double[] array = {int.Parse(stroka[0]), int.Parse(stroka[1]), int.Parse(stroka[2]), int.Parse(stroka[3])};
+Printarray(array);
+double[] Koord = Tochka(array);
+Printarray(Koord);
+
+double[] Tochka(double[] array)
+{
+    double[] koord= new double[2];
+    koord[0]=((array[3]-array[1])/(array[0]-array[2]));
+    koord[1]=array[0]*koord[0] + array[1];
+    return koord;
+}
+void Printarray(double[] array)
+{
+    int count=array.Length;
+    for (int i=0; i<count; i++)
+    {
+        Write($"{array[i]} ");
+    }
+    WriteLine("");
+}
