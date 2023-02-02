96a03a936562506f083388f5e036583ff8d168f2
zaktraxer <zaktraxer@yandex.ru>
2023-02-02 18:08:46 +0300

    Задача 66

---
+++ Zad66/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число N");
+int N = int.Parse(ReadLine()!);
+WriteLine("Введите число M");
+int M = int.Parse(ReadLine()!);
+int Sum= GetNumbersSum(N,M);
+WriteLine($"Сумма чисел от M До N равна {Sum}");
+
+int GetNumbersSum(int n, int m)
+{
+    if (n==m) return n;
+    
+    else return GetNumbersSum(n, m-1) + m;
+
+}
