b66eff3cf85ef9c5d5e4af90a534a4f9853ff0d2
zaktraxer <zaktraxer@yandex.ru>
2023-02-02 18:18:08 +0300

    Задача 68

---
+++ Zad68/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число N");
+int N = int.Parse(ReadLine()!);
+WriteLine("Введите число M");
+int M = int.Parse(ReadLine()!);
+int eee = Akk(N, M);
+WriteLine(eee);
+
+static int Akk(int n, int m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return Akk(n - 1, 1);
+    else
+        return Akk(n - 1, Akk(n, m - 1));
+}
