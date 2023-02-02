065341b16f7b6155162539a89a3fc04a19189f44
zaktraxer <zaktraxer@yandex.ru>
2023-02-02 17:55:18 +0300

    Задача 64

---
+++ Zad64/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите число");
+int N = int.Parse(ReadLine()!);
+WriteLine(GetNumbers1(N));
+
+string GetNumbers1(int m)
+{
+    return (m == 1) ? m.ToString() : $"{m},{GetNumbers1(m-1)}";
+}
